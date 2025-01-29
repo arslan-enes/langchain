a556e890fc6d06d598bb6c5536af3250f17af172
Enes Arslan <arslanenes515@gmail.com>
2025-01-30 01:57:56 +0300

    feat: prompt templates and types

--- typescript/source/app.ts
+++ typescript/source/app.ts
@@ -2,25 +2,35 @@ import { AzureChatOpenAI } from "@langchain/openai";
 import { HumanMessage } from "@langchain/core/messages";
 import dotenv from "dotenv";
 import { PromptTemplate } from "@langchain/core/prompts";
+import { ChatOllama } from "@langchain/ollama";
 
 // Load environment variables from .env file
 dotenv.config();
 
+type ChatInput = {
+  country: string;
+};
+
 async function main() {
   try {
-    const model = new AzureChatOpenAI({
+    const azure_model = new AzureChatOpenAI({
       azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
       azureOpenAIEndpoint: process.env.AZURE_OPENAI_ENDPOINT,
       azureOpenAIApiVersion: process.env.AZURE_OPENAI_API_VERSION,
       azureOpenAIApiDeploymentName: process.env.AZURE_OPENAI_DEPLOYMENT,
     });
 
-    const prompt = await PromptTemplate.fromTemplate(
+    const prompt_template = PromptTemplate.fromTemplate(
       "What colors are there in {country} flag?"
-    ).invoke({ country: "USA" });
+    );
 
-    const response = await model.invoke(prompt.value);
-    console.log(response);
+    const chain = prompt_template.pipe(azure_model);
+
+    const input: ChatInput = {
+      country: "USA",
+    };
+
+    chain.invoke(input);
   } catch (error) {
     console.error(
       "Error:",
